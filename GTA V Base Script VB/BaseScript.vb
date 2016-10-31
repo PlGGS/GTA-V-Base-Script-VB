9c5d34e7237d0eb045f154c4aa0889ebf7e65eb3
PlGGS <bbor0422@gmail.com>
2016-10-30 23:35:17 -0500

    Add project files.

---
+++ GTA V Base Script VB/BaseScript.vb
@@ -0,0 +1,27 @@
+﻿Imports GTA
+Imports System.Windows.Forms
+
+Public Class BaseScript
+    Inherits Script
+
+    'Load
+    Public Sub New()
+
+        Interval = 1
+
+    End Sub
+
+    'Update (Runs every 'Interval')
+    Public Sub Update(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Tick
+
+    End Sub
+
+    Private Sub OnKeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+    End Sub
+
+    Private Sub OnKeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+
+    End Sub
+
+End Class
---
+++ GTA V Base Script VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GTA V Base Script")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("GTA V Base Script")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3d794dcc-5d23-4a83-8d9f-edbac93f6ae4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
